1a9f01447b6bcdd9c02ae457de81167f18a16f01
DaviFrr <daviferreirabrg@gmail.com>
2024-11-04 12:13:31 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Images from './Images/developers.svg'
+import './App.css'
+import AppRoutes from './routes'
+
+function App() {
+ 
+
+  return (
+    <>
+    <AppRoutes />
+  </>
+  )
+}
+
+export default App
